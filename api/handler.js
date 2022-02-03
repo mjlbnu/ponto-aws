30c809729708e4c05633668a8dae01b51821a01c
Marcio Lisboa <mjlbnu@gmail.com>
2022-02-02 23:57:35 -0300

    Second commit

--- api/handler.js
+++ api/handler.js
@@ -1,17 +1,16 @@
 'use strict';
 const SQS = require('aws-sdk/clients/sqs');
-//const axios = require('axios');
+const axios = require('axios');
 const sqs = new SQS();
 
 module.exports.dequeue = async (event, context, callback) => {
   context.callbackWaitsForEmptyEventLoop = false;
     const ponto = JSON.parse(event.Records[0].body);
-    console.log(ponto.employeeId);
-    console.log(`EmployeeId: ${ponto.employeeId}`);
+    console.log(ponto);
     if (ponto.includedAt && ponto.includedAt.length) {
-      //const response = await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${location.city}&units=metric&APPID=08e3ad0437282f0abefa56ee74ab56af`)
-      //const answer = `Temperature - ${response.data.main.temp}°C. Humidity - ${response.data.main.humidity}%. ${response.data.weather[0].description} is expected`
-      //console.log(answer);
+      const response = await axios.get(`https://api.mockytonk.com/proxy/ab2198a3-cafd-49d5-8ace-baac64e72222?includedAt=${ponto.includedAt}&employeeId=${ponto.employeeId}&employerId=${ponto.employerId}`);
+      const answer = `Sended to legacy API: ${response.data.message}`;
+      console.log(`Answer: ${answer}`);
       callback(null, {
         statusCode: 200,
         body: JSON.stringify("success")
