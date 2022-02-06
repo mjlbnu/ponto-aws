6384a7dafb3a7be5f63c8de6e9b067a79995b750
Marcio Lisboa <mjlbnu@gmail.com>
2022-02-05 23:45:57 -0300

    This works 🎆🎉

--- api/handler.js
+++ api/handler.js
@@ -5,9 +5,7 @@ const sqs = new SQS();
 
 module.exports.dequeue = async (event, context, callback) => {
   context.callbackWaitsForEmptyEventLoop = false;
-    console.log("dequeue");
     const ponto = JSON.parse(event.Records[0].body);
-    console.log(ponto);
     if (ponto.includedAt && ponto.includedAt.length) {
       const response = await axios.get(`https://api.mockytonk.com/proxy/ab2198a3-cafd-49d5-8ace-baac64e72222?includedAt=${ponto.includedAt}&employeeId=${ponto.employeeId}&employerId=${ponto.employerId}`);
       const answer = `Sended to legacy API: ${response.data.message}`;
